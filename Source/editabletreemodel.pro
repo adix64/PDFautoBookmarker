7e39fae6c183e96469e9abc9ce98dbf1f38ef133
Alex Dinu <adix64@gmail.com>
2019-09-02 03:41:38 +0300

    added qt

---
+++ Source/editabletreemodel.pro
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<title>editabletreemodel.pro\editabletreemodel\itemviews\widgets\examples - qt/qtbase.git - Qt Base (Core, Gui, Widgets, Network, ...)</title>
+<meta name='generator' content='cgit v1.2.1'/>
+<meta name='robots' content='index, nofollow'/>
+<link rel='stylesheet' type='text/css' href='/cgit-data/cgit.css'/>
+<link rel='shortcut icon' href='/favicon.ico'/>
+<link rel='alternate' title='Atom feed' href='https://code.qt.io/cgit/qt/qtbase.git/atom/examples/widgets/itemviews/editabletreemodel/editabletreemodel.pro?h=5.13' type='application/atom+xml'/>
+<link rel='vcs-git' href='git://code.qt.io/qt/qtbase.git' title='qt/qtbase.git Git repository'/>
+<link rel='vcs-git' href='http://code.qt.io/qt/qtbase.git' title='qt/qtbase.git Git repository'/>
+<link rel='vcs-git' href='https://code.qt.io/qt/qtbase.git' title='qt/qtbase.git Git repository'/>
+</head>
+<body>
+<div id='cgit'><table id='header'>
+<tr>
+<td class='logo' rowspan='2'><a href='/cgit/'><img src='/cgit-data/logo_qt.png' alt='cgit logo'/></a></td>
+<td class='main'><a href='/cgit/'>index</a> : <a title='qt/qtbase.git' href='/cgit/qt/qtbase.git/'>qt/qtbase.git</a></td><td class='form'><form method='get'>
+<select name='h' onchange='this.form.submit();'>
+<option value='5.10'>5.10</option>
+<option value='5.11'>5.11</option>
+<option value='5.12'>5.12</option>
+<option value='5.12.1'>5.12.1</option>
+<option value='5.12.2'>5.12.2</option>
+<option value='5.12.3'>5.12.3</option>
+<option value='5.12.4'>5.12.4</option>
+<option value='5.12.5'>5.12.5</option>
+<option value='5.13' selected='selected'>5.13</option>
+<option value='5.13.0'>5.13.0</option>
+<option value='5.13.1'>5.13.1</option>
+<option value='5.14'>5.14</option>
+<option value='5.15'>5.15</option>
+<option value='5.3'>5.3</option>
+<option value='5.4'>5.4</option>
+<option value='5.5'>5.5</option>
+<option value='5.6'>5.6</option>
+<option value='5.7'>5.7</option>
+<option value='5.8'>5.8</option>
+<option value='5.9'>5.9</option>
+<option value='5.9.8'>5.9.8</option>
+<option value='dev'>dev</option>
+<option value='old/5.0'>old/5.0</option>
+<option value='old/5.1'>old/5.1</option>
+<option value='old/5.2'>old/5.2</option>
+<option value='wip/cmake'>wip/cmake</option>
+<option value='wip/highdpi'>wip/highdpi</option>
+<option value='wip/lite'>wip/lite</option>
+<option value='wip/mir'>wip/mir</option>
+<option value='wip/nacl'>wip/nacl</option>
+<option value='wip/network-test-server'>wip/network-test-server</option>
+<option value='wip/qt6'>wip/qt6</option>
+<option value='wip/remac'>wip/remac</option>
+<option value='wip/tizen'>wip/tizen</option>
+<option value='wip/webassembly'>wip/webassembly</option>
+</select> <input type='submit' value='switch'/></form></td></tr>
+<tr><td class='sub'>Qt Base (Core, Gui, Widgets, Network, ...)</td><td class='sub right'></td></tr></table>
+<table class='tabs'><tr><td>
+<a href='/cgit/qt/qtbase.git/?h=5.13'>summary</a><a href='/cgit/qt/qtbase.git/refs/?h=5.13'>refs</a><a href='/cgit/qt/qtbase.git/log/examples/widgets/itemviews/editabletreemodel/editabletreemodel.pro?h=5.13'>log</a><a class='active' href='/cgit/qt/qtbase.git/tree/examples/widgets/itemviews/editabletreemodel/editabletreemodel.pro?h=5.13'>tree</a><a href='/cgit/qt/qtbase.git/commit/examples/widgets/itemviews/editabletreemodel/editabletreemodel.pro?h=5.13'>commit</a><a href='/cgit/qt/qtbase.git/diff/examples/widgets/itemviews/editabletreemodel/editabletreemodel.pro?h=5.13'>diff</a><a href='/cgit/qt/qtbase.git/stats/examples/widgets/itemviews/editabletreemodel/editabletreemodel.pro?h=5.13'>stats</a></td><td class='form'><form class='right' method='get' action='/cgit/qt/qtbase.git/log/examples/widgets/itemviews/editabletreemodel/editabletreemodel.pro'>
+<input type='hidden' name='h' value='5.13'/><select name='qt'>
+<option value='grep'>log msg</option>
+<option value='author'>author</option>
+<option value='committer'>committer</option>
+<option value='range'>range</option>
+</select>
+<input class='txt' type='search' size='10' name='q' value=''/>
+<input type='submit' value='search'/>
+</form>
+</td></tr></table>
+<div class='path'>path: <a href='/cgit/qt/qtbase.git/tree/?h=5.13'>root</a>/<a href='/cgit/qt/qtbase.git/tree/examples?h=5.13'>examples</a>/<a href='/cgit/qt/qtbase.git/tree/examples/widgets?h=5.13'>widgets</a>/<a href='/cgit/qt/qtbase.git/tree/examples/widgets/itemviews?h=5.13'>itemviews</a>/<a href='/cgit/qt/qtbase.git/tree/examples/widgets/itemviews/editabletreemodel?h=5.13'>editabletreemodel</a>/<a href='/cgit/qt/qtbase.git/tree/examples/widgets/itemviews/editabletreemodel/editabletreemodel.pro?h=5.13'>editabletreemodel.pro</a></div><div class='content'>blob: a157a90761759f0861997d96b4190dfa97426912 (<a href='/cgit/qt/qtbase.git/plain/examples/widgets/itemviews/editabletreemodel/editabletreemodel.pro?h=5.13'>plain</a>)
+<table summary='blob content' class='blob'>
+<tr><td class='linenumbers'><pre><a id='n1' href='#n1'>1</a>
+<a id='n2' href='#n2'>2</a>
+<a id='n3' href='#n3'>3</a>
+<a id='n4' href='#n4'>4</a>
+<a id='n5' href='#n5'>5</a>
+<a id='n6' href='#n6'>6</a>
+<a id='n7' href='#n7'>7</a>
+<a id='n8' href='#n8'>8</a>
+<a id='n9' href='#n9'>9</a>
+<a id='n10' href='#n10'>10</a>
+<a id='n11' href='#n11'>11</a>
+<a id='n12' href='#n12'>12</a>
+<a id='n13' href='#n13'>13</a>
+<a id='n14' href='#n14'>14</a>
+<a id='n15' href='#n15'>15</a>
+<a id='n16' href='#n16'>16</a>
+</pre></td>
+<td class='lines'><pre><code>QT <span class="hl opt">+=</span> widgets
+<span class="hl kwd">requires</span><span class="hl opt">(</span><span class="hl kwd">qtConfig</span><span class="hl opt">(</span>treeview<span class="hl opt">))</span>
+
+FORMS       <span class="hl opt">=</span> mainwindow<span class="hl opt">.</span>ui
+HEADERS     <span class="hl opt">=</span> mainwindow<span class="hl opt">.</span>h \
+              treeitem<span class="hl opt">.</span>h \
+              treemodel<span class="hl opt">.</span>h
+RESOURCES   <span class="hl opt">=</span> editabletreemodel<span class="hl opt">.</span>qrc
+SOURCES     <span class="hl opt">=</span> mainwindow<span class="hl opt">.</span>cpp \
+              treeitem<span class="hl opt">.</span>cpp \
+              treemodel<span class="hl opt">.</span>cpp \
+              main<span class="hl opt">.</span>cpp
+
+<span class="hl ppc"># install</span>
+target<span class="hl opt">.</span>path <span class="hl opt">=</span> $$<span class="hl opt">[</span>QT_INSTALL_EXAMPLES<span class="hl opt">]/</span>widgets<span class="hl opt">/</span>itemviews<span class="hl opt">/</span>editabletreemodel
+INSTALLS <span class="hl opt">+=</span> target
+</code></pre></td></tr></table>
+</div> <!-- class=content -->
+<div class='footer'>generated by <a href='https://git.zx2c4.com/cgit/about/'>cgit v1.2.1</a> (<a href='https://git-scm.com/'>git 2.18.0</a>) at 2019-08-30 21:09:08 +0000</div>
+</div> <!-- id=cgit -->
+</body>
+</html>
---
+++ Source/ui_mainwindow.h
@@ -0,0 +1,376 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.8.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QDoubleSpinBox>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QGraphicsView>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTreeView>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *exitAction;
+    QAction *insertRowAction;
+    QAction *removeRowAction;
+    QAction *insertColumnAction;
+    QAction *removeColumnAction;
+    QAction *insertChildAction;
+    QAction *actionOpen_Folder_with_scanned_Pages;
+    QWidget *centralwidget;
+    QHBoxLayout *horizontalLayout;
+    QGroupBox *outlinesGrpBox;
+    QVBoxLayout *verticalLayout_3;
+    QTreeView *view;
+    QVBoxLayout *verticalLayout_2;
+    QGroupBox *charrsGroupBox;
+    QVBoxLayout *verticalLayout_4;
+    QLabel *maxNonAZdensityLabel;
+    QDoubleSpinBox *maxNonAZdensity;
+    QFrame *line;
+    QFrame *line_2;
+    QLabel *label_4;
+    QDoubleSpinBox *max09densitySBox;
+    QFrame *line_3;
+    QFrame *line_4;
+    QLabel *label_2;
+    QSpinBox *minCharCount;
+    QGroupBox *groupBox_4;
+    QVBoxLayout *verticalLayout_6;
+    QCheckBox *autoNestingChkBox;
+    QFrame *line_5;
+    QCheckBox *numberedHeadingsChkBox;
+    QCheckBox *latinNumeralsChkBox;
+    QCheckBox *abcListingsChkBox;
+    QGroupBox *groupBox_3;
+    QVBoxLayout *verticalLayout_5;
+    QCheckBox *deskewChkBox;
+    QCheckBox *removeImgsChkBox;
+    QVBoxLayout *verticalLayout;
+    QLabel *label_5;
+    QGraphicsView *graphicsView;
+    QHBoxLayout *horizontalLayout_2;
+    QPushButton *prevPageBtn;
+    QLabel *PageLabel;
+    QPushButton *nextPageBtn;
+    QPushButton *savePDFbutton;
+    QMenuBar *menubar;
+    QMenu *fileMenu;
+    QMenu *actionsMenu;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(765, 662);
+        MainWindow->setStyleSheet(QStringLiteral("*{color:rgb(192,192,192); background-color:rgb(64,64,64)}"));
+        MainWindow->setToolButtonStyle(Qt::ToolButtonTextOnly);
+        exitAction = new QAction(MainWindow);
+        exitAction->setObjectName(QStringLiteral("exitAction"));
+        insertRowAction = new QAction(MainWindow);
+        insertRowAction->setObjectName(QStringLiteral("insertRowAction"));
+        removeRowAction = new QAction(MainWindow);
+        removeRowAction->setObjectName(QStringLiteral("removeRowAction"));
+        insertColumnAction = new QAction(MainWindow);
+        insertColumnAction->setObjectName(QStringLiteral("insertColumnAction"));
+        removeColumnAction = new QAction(MainWindow);
+        removeColumnAction->setObjectName(QStringLiteral("removeColumnAction"));
+        insertChildAction = new QAction(MainWindow);
+        insertChildAction->setObjectName(QStringLiteral("insertChildAction"));
+        actionOpen_Folder_with_scanned_Pages = new QAction(MainWindow);
+        actionOpen_Folder_with_scanned_Pages->setObjectName(QStringLiteral("actionOpen_Folder_with_scanned_Pages"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QStringLiteral("centralwidget"));
+        horizontalLayout = new QHBoxLayout(centralwidget);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        outlinesGrpBox = new QGroupBox(centralwidget);
+        outlinesGrpBox->setObjectName(QStringLiteral("outlinesGrpBox"));
+        verticalLayout_3 = new QVBoxLayout(outlinesGrpBox);
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        view = new QTreeView(outlinesGrpBox);
+        view->setObjectName(QStringLiteral("view"));
+        view->header()->setVisible(true);
+
+        verticalLayout_3->addWidget(view);
+
+
+        horizontalLayout->addWidget(outlinesGrpBox);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        charrsGroupBox = new QGroupBox(centralwidget);
+        charrsGroupBox->setObjectName(QStringLiteral("charrsGroupBox"));
+        verticalLayout_4 = new QVBoxLayout(charrsGroupBox);
+        verticalLayout_4->setObjectName(QStringLiteral("verticalLayout_4"));
+        maxNonAZdensityLabel = new QLabel(charrsGroupBox);
+        maxNonAZdensityLabel->setObjectName(QStringLiteral("maxNonAZdensityLabel"));
+        maxNonAZdensityLabel->setAlignment(Qt::AlignBottom|Qt::AlignHCenter);
+        maxNonAZdensityLabel->setWordWrap(true);
+
+        verticalLayout_4->addWidget(maxNonAZdensityLabel);
+
+        maxNonAZdensity = new QDoubleSpinBox(charrsGroupBox);
+        maxNonAZdensity->setObjectName(QStringLiteral("maxNonAZdensity"));
+
+        verticalLayout_4->addWidget(maxNonAZdensity);
+
+        line = new QFrame(charrsGroupBox);
+        line->setObjectName(QStringLiteral("line"));
+        line->setFrameShape(QFrame::HLine);
+        line->setFrameShadow(QFrame::Sunken);
+
+        verticalLayout_4->addWidget(line);
+
+        line_2 = new QFrame(charrsGroupBox);
+        line_2->setObjectName(QStringLiteral("line_2"));
+        line_2->setFrameShape(QFrame::HLine);
+        line_2->setFrameShadow(QFrame::Sunken);
+
+        verticalLayout_4->addWidget(line_2);
+
+        label_4 = new QLabel(charrsGroupBox);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_4->addWidget(label_4);
+
+        max09densitySBox = new QDoubleSpinBox(charrsGroupBox);
+        max09densitySBox->setObjectName(QStringLiteral("max09densitySBox"));
+
+        verticalLayout_4->addWidget(max09densitySBox);
+
+        line_3 = new QFrame(charrsGroupBox);
+        line_3->setObjectName(QStringLiteral("line_3"));
+        line_3->setFrameShape(QFrame::HLine);
+        line_3->setFrameShadow(QFrame::Sunken);
+
+        verticalLayout_4->addWidget(line_3);
+
+        line_4 = new QFrame(charrsGroupBox);
+        line_4->setObjectName(QStringLiteral("line_4"));
+        line_4->setFrameShape(QFrame::HLine);
+        line_4->setFrameShadow(QFrame::Sunken);
+
+        verticalLayout_4->addWidget(line_4);
+
+        label_2 = new QLabel(charrsGroupBox);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setAlignment(Qt::AlignBottom|Qt::AlignHCenter);
+
+        verticalLayout_4->addWidget(label_2);
+
+        minCharCount = new QSpinBox(charrsGroupBox);
+        minCharCount->setObjectName(QStringLiteral("minCharCount"));
+
+        verticalLayout_4->addWidget(minCharCount);
+
+
+        verticalLayout_2->addWidget(charrsGroupBox);
+
+        groupBox_4 = new QGroupBox(centralwidget);
+        groupBox_4->setObjectName(QStringLiteral("groupBox_4"));
+        verticalLayout_6 = new QVBoxLayout(groupBox_4);
+        verticalLayout_6->setObjectName(QStringLiteral("verticalLayout_6"));
+        autoNestingChkBox = new QCheckBox(groupBox_4);
+        autoNestingChkBox->setObjectName(QStringLiteral("autoNestingChkBox"));
+
+        verticalLayout_6->addWidget(autoNestingChkBox);
+
+        line_5 = new QFrame(groupBox_4);
+        line_5->setObjectName(QStringLiteral("line_5"));
+        line_5->setFrameShape(QFrame::HLine);
+        line_5->setFrameShadow(QFrame::Sunken);
+
+        verticalLayout_6->addWidget(line_5);
+
+        numberedHeadingsChkBox = new QCheckBox(groupBox_4);
+        numberedHeadingsChkBox->setObjectName(QStringLiteral("numberedHeadingsChkBox"));
+
+        verticalLayout_6->addWidget(numberedHeadingsChkBox);
+
+        latinNumeralsChkBox = new QCheckBox(groupBox_4);
+        latinNumeralsChkBox->setObjectName(QStringLiteral("latinNumeralsChkBox"));
+
+        verticalLayout_6->addWidget(latinNumeralsChkBox);
+
+        abcListingsChkBox = new QCheckBox(groupBox_4);
+        abcListingsChkBox->setObjectName(QStringLiteral("abcListingsChkBox"));
+
+        verticalLayout_6->addWidget(abcListingsChkBox);
+
+
+        verticalLayout_2->addWidget(groupBox_4);
+
+        groupBox_3 = new QGroupBox(centralwidget);
+        groupBox_3->setObjectName(QStringLiteral("groupBox_3"));
+        verticalLayout_5 = new QVBoxLayout(groupBox_3);
+        verticalLayout_5->setObjectName(QStringLiteral("verticalLayout_5"));
+        deskewChkBox = new QCheckBox(groupBox_3);
+        deskewChkBox->setObjectName(QStringLiteral("deskewChkBox"));
+
+        verticalLayout_5->addWidget(deskewChkBox);
+
+        removeImgsChkBox = new QCheckBox(groupBox_3);
+        removeImgsChkBox->setObjectName(QStringLiteral("removeImgsChkBox"));
+
+        verticalLayout_5->addWidget(removeImgsChkBox);
+
+
+        verticalLayout_2->addWidget(groupBox_3);
+
+
+        horizontalLayout->addLayout(verticalLayout_2);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        label_5 = new QLabel(centralwidget);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
+
+        verticalLayout->addWidget(label_5);
+
+        graphicsView = new QGraphicsView(centralwidget);
+        graphicsView->setObjectName(QStringLiteral("graphicsView"));
+
+        verticalLayout->addWidget(graphicsView);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        prevPageBtn = new QPushButton(centralwidget);
+        prevPageBtn->setObjectName(QStringLiteral("prevPageBtn"));
+
+        horizontalLayout_2->addWidget(prevPageBtn);
+
+        PageLabel = new QLabel(centralwidget);
+        PageLabel->setObjectName(QStringLiteral("PageLabel"));
+
+        horizontalLayout_2->addWidget(PageLabel);
+
+        nextPageBtn = new QPushButton(centralwidget);
+        nextPageBtn->setObjectName(QStringLiteral("nextPageBtn"));
+
+        horizontalLayout_2->addWidget(nextPageBtn);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+        savePDFbutton = new QPushButton(centralwidget);
+        savePDFbutton->setObjectName(QStringLiteral("savePDFbutton"));
+        savePDFbutton->setStyleSheet(QStringLiteral(""));
+
+        verticalLayout->addWidget(savePDFbutton);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QStringLiteral("menubar"));
+        menubar->setGeometry(QRect(0, 0, 765, 26));
+        fileMenu = new QMenu(menubar);
+        fileMenu->setObjectName(QStringLiteral("fileMenu"));
+        actionsMenu = new QMenu(menubar);
+        actionsMenu->setObjectName(QStringLiteral("actionsMenu"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QStringLiteral("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(fileMenu->menuAction());
+        menubar->addAction(actionsMenu->menuAction());
+        fileMenu->addAction(exitAction);
+        fileMenu->addAction(actionOpen_Folder_with_scanned_Pages);
+        actionsMenu->addAction(insertRowAction);
+        actionsMenu->addAction(insertColumnAction);
+        actionsMenu->addSeparator();
+        actionsMenu->addAction(removeRowAction);
+        actionsMenu->addAction(removeColumnAction);
+        actionsMenu->addSeparator();
+        actionsMenu->addAction(insertChildAction);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Editable Tree Model", Q_NULLPTR));
+        exitAction->setText(QApplication::translate("MainWindow", "E&xit", Q_NULLPTR));
+        exitAction->setShortcut(QApplication::translate("MainWindow", "Ctrl+Q", Q_NULLPTR));
+        insertRowAction->setText(QApplication::translate("MainWindow", "Insert Row", Q_NULLPTR));
+        insertRowAction->setShortcut(QApplication::translate("MainWindow", "Ctrl+I, R", Q_NULLPTR));
+        removeRowAction->setText(QApplication::translate("MainWindow", "Remove Row", Q_NULLPTR));
+        removeRowAction->setShortcut(QApplication::translate("MainWindow", "Ctrl+R, R", Q_NULLPTR));
+        insertColumnAction->setText(QApplication::translate("MainWindow", "Insert Column", Q_NULLPTR));
+        insertColumnAction->setShortcut(QApplication::translate("MainWindow", "Ctrl+I, C", Q_NULLPTR));
+        removeColumnAction->setText(QApplication::translate("MainWindow", "Remove Column", Q_NULLPTR));
+        removeColumnAction->setShortcut(QApplication::translate("MainWindow", "Ctrl+R, C", Q_NULLPTR));
+        insertChildAction->setText(QApplication::translate("MainWindow", "Insert Child", Q_NULLPTR));
+        insertChildAction->setShortcut(QApplication::translate("MainWindow", "Ctrl+N", Q_NULLPTR));
+        actionOpen_Folder_with_scanned_Pages->setText(QApplication::translate("MainWindow", "Open Folder with scanned Pages", Q_NULLPTR));
+        outlinesGrpBox->setTitle(QApplication::translate("MainWindow", "Outlines", Q_NULLPTR));
+        charrsGroupBox->setTitle(QApplication::translate("MainWindow", "Characters", Q_NULLPTR));
+        maxNonAZdensityLabel->setText(QApplication::translate("MainWindow", "max density\n"
+"\302\254[a..zA..Z]", Q_NULLPTR));
+        label_4->setText(QApplication::translate("MainWindow", "max density\n"
+"[0..9]", Q_NULLPTR));
+        label_2->setText(QApplication::translate("MainWindow", "min outline\n"
+"char count", Q_NULLPTR));
+        groupBox_4->setTitle(QApplication::translate("MainWindow", "Hierarchy", Q_NULLPTR));
+        autoNestingChkBox->setText(QApplication::translate("MainWindow", "Automatic\n"
+"nesting", Q_NULLPTR));
+        numberedHeadingsChkBox->setText(QApplication::translate("MainWindow", "Numbered\n"
+"Headings", Q_NULLPTR));
+        latinNumeralsChkBox->setText(QApplication::translate("MainWindow", "Latin\n"
+"numerals", Q_NULLPTR));
+        abcListingsChkBox->setText(QApplication::translate("MainWindow", "a. b. c.\n"
+"listings", Q_NULLPTR));
+        groupBox_3->setTitle(QApplication::translate("MainWindow", "Preprocess", Q_NULLPTR));
+        deskewChkBox->setText(QApplication::translate("MainWindow", "DeSkew", Q_NULLPTR));
+        removeImgsChkBox->setText(QApplication::translate("MainWindow", "RmvImgs", Q_NULLPTR));
+        label_5->setText(QApplication::translate("MainWindow", "DOCUMENT View", Q_NULLPTR));
+        prevPageBtn->setText(QApplication::translate("MainWindow", "\342\227\204", Q_NULLPTR));
+        PageLabel->setText(QApplication::translate("MainWindow", "Page", Q_NULLPTR));
+        nextPageBtn->setText(QApplication::translate("MainWindow", "\342\226\272", Q_NULLPTR));
+        savePDFbutton->setText(QApplication::translate("MainWindow", "Save PDF", Q_NULLPTR));
+        fileMenu->setTitle(QApplication::translate("MainWindow", "&File", Q_NULLPTR));
+        actionsMenu->setTitle(QApplication::translate("MainWindow", "&Actions", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
